315c176adc065a3e42251a176f78e9fa4494575c
Ricol Wang <ricol.wang@philology.com.au>
2015-07-27 11:30:44 +1000

    added VelocityAction.

---
+++ src/au/com/rmit/Game2dEngine/action/VelocityXToAction.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package au.com.rmit.Game2dEngine.action;
+
+import au.com.rmit.Game2dEngine.node.Sprite;
+import static java.lang.Math.abs;
+
+/**
+ *
+ * @author ricolwang
+ */
+public class VelocityXToAction extends VelocityAction
+{
+    VelocityXByAction theVelocityXByAction;
+
+    double velocityXTo;
+    float velocityXToDuration;
+
+    public VelocityXToAction(Sprite theSprite)
+    {
+        if (theSprite != null)
+        {
+            this.setSprite(theSprite);
+
+            this.velocityXTo = this.theSprite.getVelocityX();
+            this.velocityXToDuration = 0;
+
+        } else
+        {
+            bComplete = true;
+        }
+    }
+
+    public void velocityXTo(double x, float duration)
+    {
+        if (duration <= 0)
+        {
+            duration = 0;
+        }
+        this.velocityXTo = x;
+        this.velocityXToDuration = abs(duration * 1000);
+    }
+
+    @Override
+    public void clearSprite()
+    {
+        this.theVelocityXByAction.clearSprite();
+        this.theVelocityXByAction = null;
+        this.theSprite = null;
+    }
+
+    @Override
+    public void perform(double runningTime)
+    {
+        if (bComplete)
+        {
+            return;
+        }
+
+        if (theVelocityXByAction == null)
+        {
+            theVelocityXByAction = new VelocityXByAction();
+            double tmpX = this.theSprite.getVelocityX();
+            theVelocityXByAction.velocityXBy(velocityXTo - tmpX, this.velocityXToDuration / 1000.0f);
+            theVelocityXByAction.setSprite(theSprite);
+        }
+
+        theVelocityXByAction.perform(runningTime);
+        if (theVelocityXByAction.bComplete)
+        {
+            bComplete = true;
+        }
+    }
+}
--- src/au/com/rmit/test/TestScene.java
+++ src/au/com/rmit/test/TestScene.java
@@ -5,13 +5,16 @@
  */
 package au.com.rmit.test;
 
+import au.com.rmit.Game2dEngine.action.VelocityXByAction;
+import au.com.rmit.Game2dEngine.action.VelocityXToAction;
+import au.com.rmit.Game2dEngine.action.VelocityYByAction;
+import au.com.rmit.Game2dEngine.action.VelocityYToAction;
 import au.com.rmit.Game2dEngine.gravity.Gravity;
 import au.com.rmit.Game2dEngine.scene.Scene;
 import au.com.rmit.test.WallSprite.WALLTYPE;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
-import static java.lang.Math.abs;
 
 /**
  *
@@ -24,7 +27,7 @@ public class TestScene extends Scene
     float delta = 0;
     float GRAVITY_VALUE = 500;
     boolean bAlreadyRun;
-    
+
     WallSprite theWall = new WallSprite(0, 0, 100, 100, 0, 0, 0);
 
     public TestScene()
@@ -80,18 +83,34 @@ public class TestScene extends Scene
                     aSprite.setBlue(255);
                     aSprite.wallType = WALLTYPE.RIGHT;
                     addSprite(aSprite);
-                    
-                    theWall.setRed(255);
-                    theWall.setLayer(1);
-                    addSprite(theWall);
+
+//                    theWall.setRed(255);
+//                    theWall.setLayer(1);
+//                    addSprite(theWall);
                 }
 
                 CircleSprite aSprite = new CircleSprite();
                 aSprite.setCentreX(e.getX());
                 aSprite.setCentreY(e.getY());
-                aSprite.setVelocityX(abs(theRandom.nextInt()) % 500);
-                aSprite.setVelocityY(abs(theRandom.nextInt()) % 500);
+                aSprite.setVelocityX(0);
+//                aSprite.setVelocityY(abs(theRandom.nextInt()) % 500);
                 addSprite(aSprite);
+
+//                VelocityXByAction aVelocityXByAction = new VelocityXByAction();
+//                aVelocityXByAction.velocityXBy(10000, 1);
+//                aSprite.addAction(aVelocityXByAction);
+
+                VelocityXToAction aVelocityXToAction = new VelocityXToAction(aSprite);
+                aVelocityXToAction.velocityXTo(10000, 1);
+                aSprite.addAction(aVelocityXToAction);
+//
+//                VelocityYByAction aVelocityYByAction = new VelocityYByAction();
+//                aVelocityYByAction.velocityYBy(10000, 1);
+//                aSprite.addAction(aVelocityYByAction);
+//
+                VelocityYToAction aVelocityYToAction = new VelocityYToAction(aSprite);
+                aVelocityYToAction.velocityYTo(10000, 1);
+                aSprite.addAction(aVelocityYToAction);
             }
 
             @Override
