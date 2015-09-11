6d627d09aff3575468431d231e749df6daa751b6
Ricol Wang <ricol.wang@philology.com.au>
2015-09-11 16:51:37 +1000

    update.

--- src/au/com/rmit/Game2dEngine/geometry/Line.java
+++ src/au/com/rmit/Game2dEngine/geometry/Line.java
@@ -5,9 +5,13 @@
  */
 package au.com.rmit.Game2dEngine.geometry;
 
+import au.com.rmit.Game2dEngine.math.QuadraticEquation;
 import java.awt.Color;
 import java.awt.Graphics2D;
+import static java.lang.Math.abs;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  *
@@ -45,18 +49,45 @@ public class Line extends Shape
         theGraphicsInTheScene.drawLine((int) start.x, (int) start.y, (int) end.x, (int) end.y);
     }
     
-    public ArrayList<Line> getSeperateLines(double distance)
+    public Set<Line> getSeperateLines(double distance)
     {
-        ArrayList<Line> lines = new ArrayList<>();
+        Set<Line> lines = new HashSet<>();
 
         double a = start.x, b = start.y, c = end.x, d = end.y;
         
-        Line line1 = new Line(start.x, start.y, 0, 0);
-        Line line2 = new Line(0, 0, end.x, end.y);
+        Point middle = new Point((a + c) / 2, (b + d) / 2);
+        if (abs(b - d) > 0.00001)
+        {
+            double t = getT(a, b, c, d);
+            double A = (1 + ((a - c) / (b - d)) * ((a - c) / (b - d)));
+            double B = ((b + d) * (a - c) / (b - d) - (a + c) - 2 * t * (a - c) / (b - d));
+            double C = ((a + c) / 2) * ((a + c) / 2) + ((b + d) / 2) * ((b + d) / 2) + t * t - (b + d) * t - distance * distance;
+            QuadraticEquation theEquation = new QuadraticEquation(A, B, C);
+            double x1 = theEquation.getX1();
+            double x2 = theEquation.getX2();
+            double y1 = this.getYfromX(a, b, c, d, x1);
+            double y2 = this.getYfromX(a, b, c, d, x2);
+            
+            middle.x = x1;
+            middle.y = y1;
+        }
+        
+        Line line1 = new Line(start.x, start.y, middle.x, middle.y);
+        Line line2 = new Line(middle.x, middle.y, end.x, end.y);
         lines.add(line1);
         lines.add(line2);
         
         return lines;
+    }
+    
+    double getT(double a, double b, double c, double d)
+    {
+        return (a * a + b * b - c * c - d * d) / (2 * (b - d));
+    }
+    
+    double getYfromX(double a, double b, double c, double d, double x)
+    {
+        return getT(a, b, c, d) - ((a - c) / (b - d)) * x;
     }
     
     @Override
